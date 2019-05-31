db332048e715e02ab71ed66ba53fa019b44fe8aa
Phafoli BOINAHANO <bphafoli@c4r23s5.wethinkcode.co.za>
2019-05-31 12:17:22 +0200

    ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: bphafoli <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/29 14:09:30 by bphafoli          #+#    #+#             */
-/*   Updated: 2019/05/29 14:21:12 by bphafoli         ###   ########.fr       */
+/*   Updated: 2019/05/31 12:16:08 by bphafoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	size_t				i;
 	unsigned char		*ptr;
-	const unsigned char	*ptr2;
+	unsigned char	*ptr2;
 
 	ptr = (unsigned char*)dst;
 	ptr2 = (unsigned char*)src;
