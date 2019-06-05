c38b7e1d9141d05bd936a7ab33d26d8f510ecd14
Phafoli BOINAHANO <bphafoli@c4r13s6.wethinkcode.co.za>
2019-06-05 13:46:26 +0200

    ft_strstr.c

---
+++ ft_strstr.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bphafoli <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/05 11:35:44 by bphafoli          #+#    #+#             */
+/*   Updated: 2019/06/05 13:31:29 by bphafoli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strstr(char *str, char *to_find)
+{
+	int i;
+	int j;
+
+	if (to_find[0] == '\0')
+		return (str);
+	i = 0;
+	while (str[i] != '\0')
+	{
+		j = 0;
+		while (to_find[j] != '\0')
+		{
+			if (str[i + j] != to_find[j])
+				break ;
+			j++;
+		}
+		if (fo_find[j] == '\0')
+			return (str + i);
+		i++;
+	}
+	return (0);
+}
