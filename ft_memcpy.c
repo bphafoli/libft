d6053eb255bc59df426de94ebcad70bef2071b15
Phafoli BOINAHANO <bphafoli@c4r13s6.wethinkcode.co.za>
2019-06-05 17:12:48 +0200

    ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,18 +6,17 @@
 /*   By: bphafoli <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/29 12:57:29 by bphafoli          #+#    #+#             */
-/*   Updated: 2019/05/29 13:43:04 by bphafoli         ###   ########.fr       */
+/*   Updated: 2019/06/05 17:08:08 by bphafoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memcpy(void *s2, const void *s1, size_t n)
+void				*ft_memcpy(void *s1, const void *s2, size_t n)
 {
-	size_t i;
-
-	unsigned char *s1;
-	unsigned char *s2;
+	size_t			i;
+	unsigned char	*s1;
+	unsigned char	*s2;
 
 	s1 = (unsigned char *)s1;
 	s2 = (unsigned char *)s2;
