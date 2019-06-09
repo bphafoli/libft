fd0c8087690d0dbe4a40a9687bcdd822342b7810
bphafoli <bphafoli@student.wethinkcode.co.za>
2019-06-09 19:55:53 +0200

    ft_putnbr_fd.c

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -10,4 +10,20 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+void	ft_putnbr_fd(int n, int fd)
+{
+	if (n <0)
+	{
+		ft_putchar_fd('-',fd);
+		ft_putnbr_fd(-n, fd);
+	}
+	if (n < 10)
+		ft_putchar_fd(n + 48, fd);
+	else
+	{
+		ft_putnbr_fd(n/10, fd);
+		ft_putnbr_fd(n%10, fd);
+	}
+}
